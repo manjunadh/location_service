1efcfc66894adeeb63dd7cd0fabad76f85e7438c
Manjunadh Veeravalli <P7111009@NESSINBLR.COM>
2017-03-23 15:01:07 +0530

    changed Storeservice

--- src/main/java/com/fgl/repository/StoreRepository.java
+++ src/main/java/com/fgl/repository/StoreRepository.java
@@ -1,21 +1,17 @@
 package com.fgl.repository;
 
-
 import java.util.List;
-
-import org.springframework.data.geo.Distance;
-import org.springframework.data.geo.Point;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
 
 import com.fgl.model.Store;
 
-
 public interface StoreRepository extends CrudRepository<Store, String> {
 
-    List<Store> findByProvince(@Param("province") String province);
-    List<Store> findByPointNear(@Param("location") Point location, @Param("distance") Distance distance);
+	List<Store> findByProvince(@Param("province") String province);
 
-
+	List<Store> findByPointNear(@Param("location") Point location, @Param("distance") Distance distance);
 
 }
--- src/main/java/com/fgl/services/StoreServiceImpl.java
+++ src/main/java/com/fgl/services/StoreServiceImpl.java
@@ -10,97 +10,17 @@ import org.springframework.data.geo.Point;
 
 import com.fgl.model.Store;
 import com.fgl.repository.StoreRepository;
+import com.fgl.repository.StoreRepositoryImpl;
 
 public class StoreServiceImpl implements StoreService {
 	private static final Distance distance = new Distance(50, Metrics.KILOMETERS);
 	@Autowired
 	private StoreRepository storeRepository;
 
-@Bean
-StoreRepository storeRepository(){
-	return new StoreRepository() {
-		
-		@Override
-		public <S extends Store> Iterable<S> save(Iterable<S> entities) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public <S extends Store> S save(S entity) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Store findOne(String id) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Iterable<Store> findAll(Iterable<String> ids) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Iterable<Store> findAll() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public boolean exists(String id) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public void deleteAll() {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void delete(Iterable<? extends Store> entities) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void delete(Store entity) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void delete(String id) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public long count() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public List<Store> findByProvince(String province) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public List<Store> findByPointNear(Point location, Distance distance) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	};
-}
-
-
+	@Bean
+	StoreRepository storeRepository() {
+		return new StoreRepositoryImpl();
+	}
 
 	@Override
 	public Store getStore(String storeNumber) {
@@ -121,9 +41,9 @@ StoreRepository storeRepository(){
 
 	@Override
 	public List<Store> getAllStoresNearby(String storeNumber) {
-		Store store= storeRepository.findOne(storeNumber);
-		return storeRepository.findByPointNear(store.getLocation(),distance);
-		
+		Store store = storeRepository.findOne(storeNumber);
+		return storeRepository.findByPointNear(store.getLocation(), distance);
+
 	}
 
 }
