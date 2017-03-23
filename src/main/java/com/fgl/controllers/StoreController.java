302a7b1e32af951527eabcb0bc81b6906de1e0a3
Manjunadh Veeravalli <P7111009@NESSINBLR.COM>
2017-03-23 16:04:40 +0530

    changed Storeservice

--- src/main/java/com/fgl/controllers/StoreController.java
+++ src/main/java/com/fgl/controllers/StoreController.java
@@ -7,17 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.fgl.model.Store;
 import com.fgl.services.StoreService;
 import com.fgl.services.StoreServiceImpl;
 
-/**
- * Created by rhett on 2017-03-15.
- */
 @RestController
 public class StoreController {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(StoreController.class);
 	@Autowired
 	private StoreService storeService;
 
@@ -29,16 +27,22 @@ public class StoreController {
 	// single store
 	@RequestMapping("/location/{storeNumber}")
 	public Store getStore(@PathVariable String storeNumber) {
-
+		LOGGER.info("hitting /location/" + storeNumber + " controller");
 		Store store = storeService.getStore(storeNumber);
 		return store;
 
 	}
 
+	@RequestMapping("/")
+	public String home() {
+		LOGGER.info("hitting /location/home");
+		return "home page";
+	}
+
 	// all stores
 	@RequestMapping("/location/")
 	public List<Store> getAllStores() {
-
+		LOGGER.info("hitting /location/ controller");
 		List<Store> stores = storeService.getAllStores();
 		return stores;
 
@@ -47,19 +51,19 @@ public class StoreController {
 	// all stores by province
 	@RequestMapping("/location/province/{province}")
 	public List<Store> getAllStoresByProvince(@PathVariable String province) {
-
+		LOGGER.info("hitting province /location/" + province + " controller");
 		List<Store> stores = storeService.getAllStoresForProvince(province);
 		return stores;
 
 	}
 
 	// all stores nearby (default 5km)
-//	@RequestMapping("/location/nearby/{storeNumber}")
-//	public List<Store> getAllStoresNearby(@PathVariable String storeNumber) {
-//
-//		List<Store> stores = storeService.getAllStoresNearby(storeNumber);
-//		return stores;
+	// @RequestMapping("/location/nearby/{storeNumber}")
+	// public List<Store> getAllStoresNearby(@PathVariable String storeNumber) {
+	//
+	// List<Store> stores = storeService.getAllStoresNearby(storeNumber);
+	// return stores;
 
-//	}
+	// }
 
 }
--- src/main/java/com/fgl/services/StoreServiceImpl.java
+++ src/main/java/com/fgl/services/StoreServiceImpl.java
@@ -6,13 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Metrics;
-import org.springframework.data.geo.Point;
-
+//import org.springframework.data.geo.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.fgl.model.Store;
 import com.fgl.repository.StoreRepository;
 import com.fgl.repository.StoreRepositoryImpl;
 
 public class StoreServiceImpl implements StoreService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(StoreServiceImpl.class);
 	private static final Distance distance = new Distance(50, Metrics.KILOMETERS);
 	@Autowired
 	private StoreRepository storeRepository;
@@ -24,11 +26,13 @@ public class StoreServiceImpl implements StoreService {
 
 	@Override
 	public Store getStore(String storeNumber) {
+		LOGGER.info("get by store number service" + storeNumber);
 		return storeRepository.findOne(storeNumber);
 	}
 
 	@Override
 	public List<Store> getAllStores() {
+		LOGGER.info("get all stores service");
 		List<Store> stores = new ArrayList<>();
 		storeRepository.findAll().forEach(stores::add);
 		return stores;
@@ -36,6 +40,7 @@ public class StoreServiceImpl implements StoreService {
 
 	@Override
 	public List<Store> getAllStoresForProvince(String province) {
+		LOGGER.info("get stores by provience service");
 		return storeRepository.findByProvince(province);
 	}
 
